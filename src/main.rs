f86b6a4b2febef448763f4a2c67fd4e1338dcf45
scurest <scurest@users.noreply.github.com>
2022-09-05 11:33:15 -0500

    Optimize conversion by reusing buffer; correct comment

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,17 @@ fn main() {
     println!("Path to file: {}", &path);
 
     // Read the file
-    let buf = fs::read(&path).expect("Error reading the file!");
+    let mut buf = fs::read(&path).expect("Error reading the file!");
 
-    // Change first 16 bytes of the RPG Maker V Picture header to 8 bytes of the PNG header
-    let png_head: Vec<u8> = vec![0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52];
-    let result: Vec<u8> = [&png_head[..], &buf[32..]].concat();
+    // Remove 16 bytes, then change the next 16 bytes to PNG header
+    let png = &mut buf[16..];
+    let png_head = b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR";
+    (&mut png[..16]).copy_from_slice(png_head);
 
     // Write the new file
     let end_filename = &mut path;
     end_filename.push_str(".png");
-    fs::write(&end_filename, &result).expect("Error writing the file!");
+    fs::write(&end_filename, png).expect("Error writing the file!");
 
     // println!("{:?}", &buf);
     println!("Exported to {}", end_filename);
